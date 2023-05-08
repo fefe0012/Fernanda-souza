3d1f18039876c5af22ded7e504bb5d6c4e61cb6b
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-08 14:49:49 -0300

    Update luv.java

--- luv.java
+++ luv.java
@@ -4,4 +4,13 @@ Fernanda Souza
 7/05
 */
 import java.util.Scanner;
-public class
+public class luv{
+    public static void main(String[]args){
+        Scanner ler = new Scanner(System.in);
+        int raio;
+        System.out.print("insira um numero para o raio do circulo:");
+        raio = ler.nextInt();
+        System.out.print("\nO perímetro do raio inserido é\n");
+        System.out.print(6.28*raio);
+    }
+}
