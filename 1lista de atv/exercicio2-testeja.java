c3befc8c5ab9de30027856a7c74cb44af41e0502
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-14 22:27:48 -0300

    Add files via upload

---
+++ 1lista de atv/exercicio2-testeja.java
@@ -0,0 +1,22 @@
+/*
+recebe 4 numeros e calcula o quadrado de cada e soma-os
+Fernanda Souza
+07/05/23
+*/
+import java.util.Scanner;
+public class testeja{
+    public static void main(String[]args){
+        Scanner ler = new Scanner(System.in);
+        int n1,n2,n3,n4;
+        System.out.print("digite um numero");
+        n1 = ler.nextInt();
+        System.out.print("digite outro numero");
+        n2 = ler.nextInt();
+        System.out.print("digite outro numero");
+        n3 = ler.nextInt();
+        System.out.print("digite outro numero");
+        n4 = ler.nextInt();
+        System.out.print("\nResultado:\n");
+        System.out.print((n1^2)+(n2^2)+(n3^2)+(n4^2));
+    }
+}
