7806a7e5b2c09708a5015ce225703560b3dbe3f3
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-14 23:39:49 -0300

    Update exercicio3-javtext.java

--- 1lista de atv/exercicio3-javtext.java
+++ 1lista de atv/exercicio3-javtext.java
@@ -3,15 +3,18 @@ converte um valor em real para dolar
 Fernanda Souza 
 07/05  
 */
+// importando a class scanner
 import java.util.Scanner;
 public class javtext{
     public static void main(String[]args){
         Scanner ler = new Scanner(System.in);
+//cria as variaveis 
         double valorr, valord;
         System.out.print("digite em valor em real");
         valorr = ler.nextDouble();
         System.out.print("\nO valor em dolar é\n");
         System.out.print(valorr/4.95);
+ // mostra para o usuario o resultado
 
     }
 }
