7cf306c4db46f7321831541753f7818738080124
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-14 23:24:58 -0300

    Update exercicio2-testeja.java

--- 1lista de atv/exercicio2-testeja.java
+++ 1lista de atv/exercicio2-testeja.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class testeja{
     public static void main(String[]args){
         Scanner ler = new Scanner(System.in);
+// cria as variaveis 
         int n1,n2,n3,n4;
         System.out.print("digite um numero");
         n1 = ler.nextInt();
@@ -18,5 +19,6 @@ public class testeja{
         n4 = ler.nextInt();
         System.out.print("\nResultado:\n");
         System.out.print((n1^2)+(n2^2)+(n3^2)+(n4^2));
+// mostra o resultado 
     }
 }
