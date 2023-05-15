4e7332cd8e3d15f99c3c67c5cc49ffc858d57957
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-14 23:46:39 -0300

    Update excercicio5-luv.java

--- 1lista de atv/excercicio5-luv.java
+++ 1lista de atv/excercicio5-luv.java
@@ -3,14 +3,17 @@ calcula um perímetro a partir de um r inserido pelo usuario
 Fernanda Souza 
 7/05
 */
+// importa a classe scanner
 import java.util.Scanner;
 public class luv{
     public static void main(String[]args){
         Scanner ler = new Scanner(System.in);
+// cria a variavel
         int raio;
         System.out.print("insira um numero para o raio do circulo:");
         raio = ler.nextInt();
         System.out.print("\nO perímetro do raio inserido é\n");
         System.out.print(6.28*raio);
+ // mostra o resultado
     }
 }
