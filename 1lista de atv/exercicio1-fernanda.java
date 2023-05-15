960fbe02872b85493ef53a0726cc9cfed8b3fd66
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-14 22:47:39 -0300

    Update exercicio1-fernanda.java

--- 1lista de atv/exercicio1-fernanda.java
+++ 1lista de atv/exercicio1-fernanda.java
@@ -4,20 +4,26 @@
 *autor: Fernanda Souza
 *data:05/05/2023
 */
+// importa a classe scanner
 import java.util.Scanner;
 public class fernanda.java {
     public static void main(String args[]){
         Scanner ler = new Scanner(System.in)
+//cria as variaveis tipo real
         double n1, n2, n3, n4, res;
+// inicializa as variaveis
+        n1 = n2 = n3 = n4 =0;
         System.out.print(insira um numero );
-        n1 = ler.nextDouble();
+        n1 = ler.nextDoubleln();
         System.out.print(insira outro valor);
-        n2 = ler.nextDouble();
+        n2 = ler.nextDoubleln();
         System.out.print(insira um outro valor);
-        n3 = ler.nextDouble();
+        n3 = ler.nextDoubleln();
         System.out.print(insira um outro numero);
-        n4 = ler.nextDouble();
+        n4 = ler.nextDoubleln();
         System.out.print("\nResultado:\n");
         System.out.print(n1+n2+n3+n4);
+ //mostra o resultado ao usuario
+        
     }
 }
