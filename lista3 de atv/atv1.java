fa8afef79788afd9247f076c6b220f0f7c3a1f9e
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-16 17:03:19 -0300

    Add files via upload

---
+++ lista3 de atv/atv1.java
@@ -0,0 +1,17 @@
+/*
+ * descrição:mostra todos os numeeros de 0 a 1000
+ * autor:Fernanda Souza
+ * data:16/05
+ */
+public class atv1{
+    public static void main(String[]args){
+        //incializa as variaveis 
+        int cont =0;
+        while(cont<=1000){
+            System.out.println(cont);
+            //aqui o codigo é instruído a mostrar o resultado
+            cont++;
+            
+        }
+    }
+}
