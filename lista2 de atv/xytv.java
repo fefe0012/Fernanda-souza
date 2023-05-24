6cb5ecfb258f3a9dc9dc368163062d8b9301333d
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-24 11:38:49 -0300

    Add files via upload

---
+++ lista2 de atv/xytv.java
@@ -0,0 +1,35 @@
+/*
+ * descrição: troca de valores 
+ * autor: Fernanda Soza
+ * data:21/05
+ */
+ //importa a classe Scanner
+ import java.util.Scanner;
+ // classe principal
+ public class xytv{
+    // metodo que sera executa primeiro
+    public static void main(String[]args){
+           // instancia um objeto "leitor"
+           Scanner ler = new Scanner (System.in);
+           //cria as variaveis
+           int n1 = 0;
+           int n2 = 0;
+           int n3 = 0; 
+           int n4 = 0;
+           //inicio das variaveis 
+           
+           System.out.println("insira um valor");
+           n1 = ler.nextInt();
+           System.out.println("insira outro valor");
+           n2 = ler.nextInt();
+          n3 = n1;
+          n1 = n2; 
+          System.out.println("o valor de " + n1 + " é "  + n2);
+          n4 = n2;
+          n4 = n3;
+           
+           
+          
+           System.out.println("o valor de " + n4 +  " é "  + n3);
+    }
+ }
