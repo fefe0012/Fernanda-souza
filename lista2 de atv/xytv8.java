64d54a907ee25074f04bb813ff6ace72920d71f6
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-29 13:22:41 -0300

    Add files via upload

---
+++ lista2 de atv/xytv8.java
@@ -0,0 +1,51 @@
+/* descrição: um aplicativo que visa tirar dinheiro de alguem com problemas piscologico 
+ * autor:Fernanda Souza
+ * data:29/05
+ */
+import java.util.Scanner;
+
+public class xytv8{
+
+public static void main(String[] args){
+ 
+Scanner ler= new Scanner (System.in );
+
+int k2= 0;
+char n1;
+double tz=1.50;
+double tv=0.0;
+String j1;
+
+// interage com o usuário 
+
+System.out.println ("Olá,seu dia foi bom?");
+System.out.println ("se sim digite S");
+System.out.println("se não digite N");
+// ler resposta do usuário 
+ n1 = ler.NextChar();
+if ( n1==s) { 
+      System.out.println ("que bom");
+        } else  {
+                   System.out.println ("poxa");
+                 }
+
+ System.out.println ( "digite o pq…");
+j1= ler.NextString ();
+System.out.println ( "de 1 a 10 digite o quanto lê ajudou a desabafar sobre");
+
+k2=ler.NextInt();
+
+System.out.println ( "sua nota é" + k2+ "para nosso app.");
+
+/* valor da promoção tem em base a nota que o cliente da para o app 
+ou seja quanto maior a nota mais caro fica já que o cliente ta satisfeito com o app 
+então ele se contentará com o preço mais salgado, porém visando a aproximação do cliente insatisfeito ele
+pagará mais barato por questões de querer aproxima esse cliente do app */
+
+Tv= tz*k2;
+System.out.println("PROMOÇÃO");
+
+System.out.println ("pagando" +tv+ "vc tem direito a consultas com um psicólogo online");
+
+ }
+}
