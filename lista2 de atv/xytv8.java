a15f1875a8c5cd7449c0e191cd2ad39eec333807
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-29 16:49:48 -0300

    Update xytv8.java

--- lista2 de atv/xytv8.java
+++ lista2 de atv/xytv8.java
@@ -11,10 +11,10 @@ public static void main(String[] args){
 Scanner ler= new Scanner (System.in );
 
 int k2= 0;
-char n1;
+String n1;
 double tz=1.50;
 double tv=0.0;
-String j1;
+String k9;
 
 // interage com o usuário 
 
@@ -22,30 +22,32 @@ System.out.println ("Olá,seu dia foi bom?");
 System.out.println ("se sim digite S");
 System.out.println("se não digite N");
 // ler resposta do usuário 
- n1 = ler.NextChar();
-if ( n1==s) { 
+ n1 = ler.nextLine();
+if (n1.equals("s")) { 
       System.out.println ("que bom");
         } else  {
                    System.out.println ("poxa");
+                   System.out.println ( "digite o pq…");
+                   k9= ler.nextLine ();
                  }
 
- System.out.println ( "digite o pq…");
-j1= ler.NextString ();
+
+
 System.out.println ( "de 1 a 10 digite o quanto lê ajudou a desabafar sobre");
 
-k2=ler.NextInt();
+k2=ler.nextInt();
 
-System.out.println ( "sua nota é" + k2+ "para nosso app.");
+System.out.println ( "sua nota é " + k2 + " para nosso app.");
 
 /* valor da promoção tem em base a nota que o cliente da para o app 
 ou seja quanto maior a nota mais caro fica já que o cliente ta satisfeito com o app 
 então ele se contentará com o preço mais salgado, porém visando a aproximação do cliente insatisfeito ele
 pagará mais barato por questões de querer aproxima esse cliente do app */
 
-Tv= tz*k2;
+tv= tz*k2;
 System.out.println("PROMOÇÃO");
 
-System.out.println ("pagando" +tv+ "vc tem direito a consultas com um psicólogo online");
+System.out.println ("pagando " + tv + " vc tem direito a consultas com um psicólogo online");
 
  }
 }
