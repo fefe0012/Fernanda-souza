505b3980c380396146bd0d3034420c24d23fbb98
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-29 12:58:17 -0300

    Add files via upload

---
+++ lista2 de atv/xytv10.java
@@ -0,0 +1,26 @@
+/*
+ * descrição: Recebe um valor e mostra se é impar ou par
+ * autor:Fernanda
+ * data:24/05
+ */
+ //importa a classe Scanner
+import java.util.Scanner;
+ public class xyt5{
+     // metodo que sera executa primeiro
+     public static void main(String[]args){
+        // instancia um objeto "leitor"
+        Scanner ler = new Scanner (System.in);
+        // cria e inicializa as variaveis
+        int n1 = 0;
+        // 
+        System.out.println("Digite um numero");
+        n1 = ler.nextInt();
+        
+        /*excecuta o codigo e logo em seguida mostra o resultado para o usuario */
+       if ( n1%=0 ){
+           System.out.println("É PAR");
+            
+       } else System.out.println("É IMPAR");
+        
+     }
+ } 
---
+++ lista2 de atv/xytv7.java
@@ -0,0 +1,25 @@
+/*
+ * descrição: Recebe um valor, caso esteja entre 94 e 456 mostra "acertou" senao mostra "tenta denovo"
+ * autor:Fernanda
+ * data:24/05
+ */
+ //importa a classe Scanner
+ import java.util.Scanner;
+ public class xyt5{
+     // metodo que sera executa primeiro
+     public static void main(String[]args){
+        // instancia um objeto "leitor"
+        Scanner ler = new Scanner (System.in);
+        // cria e inicializa as variaveis
+        int n1 = 0;
+        // 
+        System.out.println("Digite um numero");
+        n1 = ler.nextInt();
+        
+        /*excecuta o codigo e logo em seguida mostra o resultado para o usuario */
+       if ( n1 == 94 ){
+           System.out.println("ACERTOU");
+       } else System.out.println("TENTE DENOVO");
+        
+     }
+ } 
