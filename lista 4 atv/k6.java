ce15c234f18397f256670cdf32f4de7aac43d930
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-30 15:57:57 -0300

    Update k6.java

--- lista 4 atv/k6.java
+++ lista 4 atv/k6.java
@@ -1,19 +1,25 @@
 /*
- * descriação:soma somente 10 numero positivos e mostra a sua raiz,
- * ignora qualquer numero negativo que o usuario inserir
- * autor:Fernanda Souza 
+ * descrição:soma todos os numeros cujo quadrado é divisível por 4.
+ * autor:Fernanda Souza
  * data:30/05
  */
- import java.util.Scanner;
-  public class k6{
-    public static void main(String[]args){
-        Scanner ler = new Scanner(System.in);
-        int contador = 0;
-        double somadora = 0.0;
 
-        do {
-            System.out.println("a soma é "+ somadora);
-            n1++;
-        }while(n1=10);
+public class k1{
+    public static void main(String[]args){
+       
+        //incializa as variaveis 
+        int nt = 10; 
+        double soma= 0;
+        for(int cont = 1; cont <= nt; cont ++){
+            // acha o quadrado do valor 
+             double quad = Math.pow(cont,2);
+            if( quad % 4 ==0){
+               soma = soma + cont ;
+               System.out.println(" o  valor do quadrado de "+ cont "é: "+ quad)
+              System.out.println(" o valor da soma é: " +soma );
+            } 
+
+            //aqui o codigo é instruído a mostrar o resultado  
+        }
     }
-  }
+}
