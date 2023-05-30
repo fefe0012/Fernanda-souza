248314fcf35135ab08a1685df382302530db77a9
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-30 17:50:39 -0300

    Update k6.java

--- lista 4 atv/k6.java
+++ lista 4 atv/k6.java
@@ -3,23 +3,23 @@
  * autor:Fernanda Souza
  * data:30/05
  */
-
-public class k1{
+ import java.util.Scanner;
+  public class k6{
     public static void main(String[]args){
-       
-        //incializa as variaveis 
-        int nt = 10; 
-        double soma= 0;
-        for(int cont = 1; cont <= nt; cont ++){
-            // acha o quadrado do valor 
-             double quad = Math.pow(cont,2);
-            if( quad % 4 ==0){
-               soma = soma + cont ;
-               System.out.println(" o  valor do quadrado de "+ cont "é: "+ quad)
-              System.out.println(" o valor da soma é: " +soma );
-            } 
-
-            //aqui o codigo é instruído a mostrar o resultado  
-        }
+        Scanner ler = new Scanner(System.in);
+     int quant = 1;
+     int soma = 0;
+     int valor = 0;
+     while (quant <= 4){
+         System.out.println("Digite o valor" );
+         valor = ler.nextInt();
+         quant ++;
+         if ( valor > 0){
+            soma += valor;
+            System.out.println("a soma dos valores inseridos é: " + soma );
+         }
+     }
+    double raiz = Math.sqrt(soma);
+    System.out.println( " a raiz é: " + raiz);
     }
-}
+  }
