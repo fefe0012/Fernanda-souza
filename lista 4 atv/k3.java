93ee2d7a934ccfe1d8a113dedb50e19554dd7393
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-30 17:14:38 -0300

    Add files via upload

---
+++ lista 4 atv/k3.java
@@ -0,0 +1,7 @@
+/*
+*descrição: gera 50 numeros inteiro e soma eles em uma variavel, e gera 50 numeros reias aleatorios
+*autor:Fernanda Souza
+*data:30/05
+*/
+import java.util.Scanner;
+ public class 
