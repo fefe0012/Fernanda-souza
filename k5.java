84aa27e912487c53bfb9d7d572f5ff43520014f6
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-30 11:47:43 -0300

    Add files via upload

---
+++ k5.java
@@ -0,0 +1,19 @@
+/*
+ * descrição:conta de 10 ate 0 e mostra fim 
+ * autor:Fernanda Souza
+ * data:30/05
+ */
+public class k5{
+    public static void main(String[]args){
+        //incializa as variaveis 
+        int cont =0;
+        for(cont=10;cont>=0;cont--){
+            System.out.println(cont);
+            //aqui o codigo é instruído a mostrar o resultado
+        
+            
+        }
+        System.out.println("FIM");
+    }
+}
+
