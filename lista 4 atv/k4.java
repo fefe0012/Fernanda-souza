7babd4b8e487977809433f944effe0d026bb9b37
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-30 13:11:27 -0300

    Add files via upload

---
+++ lista 4 atv/k4.java
@@ -0,0 +1,20 @@
+/*
+ * descrição:recebe um texto e ve se a quantidade de caracter é maior que 
+ * 10 mostra que a pessoa digitou um palavrão, e se menor que 10 digitou uma palavrinha.
+ * autor:fernanda souza
+ * data:30/05
+ */
+import java.util.Scanner;
+public class k4{
+    public static void main(String[]args){
+        Scanner ler = new Scanner(System.in);
+        char n1;
+        System.out.println("digite um texto ou letras aleatorias.");
+        n1 = ler.nextLine().charAt(0);
+        if(n1 >= 10 ){
+            System.out.println(" vc digitou um palavrão!");
+        }      else (n1 <10 ){
+                 System.out.println(" vc digitou uma palavrinha");           
+        }
+    }
+}
