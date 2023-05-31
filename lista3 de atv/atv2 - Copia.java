9b990a296a6f081b53bd8b393a95f69446b991ac
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-30 23:28:11 -0300

    Update atv2 - Copia.java

--- lista3 de atv/atv2 - Copia.java
+++ lista3 de atv/atv2 - Copia.java
@@ -8,12 +8,16 @@
   public class atv2{
     public static void main(String[]args){
         Scanner ler = new Scanner(System.in);
+     //cria as variaveis 
         int n1,n2;
+     // faz uma interação com o usuario 
         System.out.println("insria um numero");
         n1 = ler.nextInt();
         System.out.println("insria um numero");
+     // termina de ler o que o usuario inseriu 
         n2 = ler.nextInt();
         System.out.println();
+     // excuta e mostra o resultado 
         while( n1 < n2 ){
             n1++;
             n2--;
