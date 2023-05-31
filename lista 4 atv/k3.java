97541e690ecd0170eee311bbbd274e9779a6adec
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-30 23:36:21 -0300

    Update k3.java

--- lista 4 atv/k3.java
+++ lista 4 atv/k3.java
@@ -2,15 +2,17 @@
 *descrição: gera 50 numeros inteiro e soma eles em uma variavel, e gera 50 numeros reias aleatorios
 *autor:Fernanda Souza
 *data:30/05
-*/
+*/ 
+//importa a classe scanner 
 import java.util.Scanner;
  import java.util.Random;
  public class k3{
     public  static void main(String[]args){
 
         Scanner ler = new Scanner(System.in);
-
+// impota a class random
         Random gerador = new Random();
+     // cria as variaveis 
         int w = 0; 
         int o = 0;
         for( int p = 0 ; p < 50; p++) {
@@ -27,7 +29,7 @@ import java.util.Scanner;
         for( double f = 0.0 ; f < 50.0; f++) {
             r = gerador.nextDouble(50.0);
             
-            System.out.println("ja  gerei um numero ate 50.0-: " + r);
+            System.out.println("ja  gerei um numero ate 50.0: " + r);
             
             a += r;
             System.out.println(" soma dos numeros inteiros são "+ a);
