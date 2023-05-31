181556b15b28b5b09046ca7503664fed941b92cf
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-30 23:29:41 -0300

    Update xp3.java

--- lista3 de atv/xp3.java
+++ lista3 de atv/xp3.java
@@ -3,23 +3,25 @@
  * autor:Fernanda Souza 
  * data:30/05
  */
+//importa a classe scanner
 import java.util.Scanner;
 
 public class xp3{
     public static void main(String[]args){
         Scanner ler = new Scanner(System.in);
-
+// cria as variaveis
         int somadora = 0;
         int j = 0 ;
-
+//inicializa o metodo for 
          for( int contador = 0; contador< 10; contador++){
             System.out.println("insira um valor: ");
             somadora = ler.nextInt();
             j += somadora;
          }
+        // conclui a soma e começa a excutar a media 
          double  media = 0.0;
          media = j /10;
-
+ // mostra o resultado
          System.out.println("a media é : " + media );
 
     }
