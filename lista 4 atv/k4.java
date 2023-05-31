0a4207e9fe6df69df8534073a5cd0a0441ad128a
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-30 23:39:58 -0300

    Update k4.java

--- lista 4 atv/k4.java
+++ lista 4 atv/k4.java
@@ -4,14 +4,18 @@
  * autor:fernanda souza
  * data:30/05
  */
+// importa a class scanner
 import java.util.Scanner;
 public class k4{
     public static void main(String[]args){
         Scanner ler = new Scanner(System.in);
-        
+        // cria as variaveis 
         int n1;
+        // da instruções para o usuario
         System.out.println("digite um texto ou letras aleatorias.");
+        // ler oq o usuario digitou
         n1 = ler.nextLine().length();
+        // mostra para o usuario se seu texto tem mais de 10 letras, ou se menos de uma maneira inesperada
         if(n1 >= 10 ){
              System.out.println(" vc digitou um palavrão!");
             
