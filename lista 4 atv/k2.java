0107e36d1744dcb23c98d74adf956783a50226c2
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-30 23:32:29 -0300

    Update k2.java

--- lista 4 atv/k2.java
+++ lista 4 atv/k2.java
@@ -7,15 +7,18 @@ import java.util.Scanner;
 public class k2{
     public static void main(String[]args){
         Scanner ler = new Scanner(System.in);
+        // cria as variaveis 
         int n1 = 0;
         int n2 = 0; 
         int n3 = 0;
         int res= 0;
+        
         System.out.println("digite 3 numeros ");
         n1 = ler.nextInt();
         n2 = ler.nextInt();
         n3 = ler.nextInt();
         res = n1+ n2+ n3;
+        // recebe 3 numeros e se nao for igual o que estar na condição ele executa o metodo while ate o usuario chegar no resultado
          while ( res !=48 && res !=71 ){
             System.out.println("digite 3 numeros ");
               n1 = ler.nextInt();
