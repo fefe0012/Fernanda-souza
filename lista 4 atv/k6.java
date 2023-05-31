39f8fd116a6d344732253a4e3f2067d6c7c8b595
souzsa <130611983+souzsa@users.noreply.github.com>
2023-05-30 23:42:07 -0300

    Update k6.java

--- lista 4 atv/k6.java
+++ lista 4 atv/k6.java
@@ -3,13 +3,16 @@
  * autor:Fernanda Souza
  * data:30/05
  */
+// importa a class scanner
  import java.util.Scanner;
   public class k6{
     public static void main(String[]args){
         Scanner ler = new Scanner(System.in);
+     // cria as variaveis 
      int quant = 1;
      int soma = 0;
      int valor = 0;
+     // inicializa o metodo while 
      while (quant <= 4){
          System.out.println("Digite o valor" );
          valor = ler.nextInt();
@@ -19,6 +22,7 @@
             System.out.println("a soma dos valores inseridos é: " + soma );
          }
      }
+     // depois de somar tudo com o laço while ele vem e mostra a raiz do numero divisivel por 4
     double raiz = Math.sqrt(soma);
     System.out.println( " a raiz é: " + raiz);
     }
